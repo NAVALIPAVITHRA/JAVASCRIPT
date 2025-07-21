8a0f405e1f1984235c26a823a0ccb63d4a15a6ca
NAVALIPAVITHRA <navalipavithra25@gmail.com>
2025-07-21 08:49:31 +0530

    js day2

---
+++ DAY2/foreach.js
@@ -0,0 +1,16 @@
+//ARRAY METHODS -forEach 
+arr=[
+    {
+        name:"Sagarhafs",
+        age:23
+    },
+    {name:"Sagaegrsfhrhafs",
+        age:24
+
+    }
+];
+arr.forEach((student)=>{
+
+console.log(`${student.name} and ${student.age}`);
+}
+);
---
+++ DAY2/map.js
@@ -0,0 +1,5 @@
+//map
+let num=[1,2,3,4];
+let newArr = num.map((el)=>{
+    return el*2;
+});
