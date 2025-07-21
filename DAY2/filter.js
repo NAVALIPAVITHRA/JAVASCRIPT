2f79f34b8ddd372ec4852c485d7bb1fc5a3dd166
NAVALIPAVITHRA <navalipavithra25@gmail.com>
2025-07-21 11:41:31 +0530

    js day2

---
+++ DAY2/filter.js
@@ -0,0 +1,4 @@
+//filter
+let num=[1,2,3,4,5,6,7,7,89];
+let even = num.filter((el)=>(el%2==0));
+console.log(even);
---
+++ DAY2/spreaddata.js
@@ -0,0 +1,7 @@
+const data={
+    email:"navali@gmail.com",
+    id:"abc"
+
+};
+const copy={...data,gender:"g"};
+console.log(copy);
