700eb6a3e1e847a93ab0d272ed96e3c7de2beacc
NAVALIPAVITHRA <navalipavithra25@gmail.com>
2025-07-22 20:25:44 +0530

    DOM model

---
+++ DAY4/app.js
@@ -0,0 +1,9 @@
+let para = document.createElement('p');
+para.innerText="sdhrdfnhdfc";
+document.querySelector('body').append(para);
+para.classList.add("red");
+
+let h3=document.createElement("h3");
+h3.innerText="ensgirnbfi";
+document.querySelector("body").append(h3);
+h3.classList.add("red");
